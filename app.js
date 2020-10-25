bae2a8ccf17077a994ccab26b0b0de6dcc0c1df6
Ikraam <IbnDaanis@Gmail.com>
2020-10-25 14:40:34 +0000

    Added filter by region feature

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const darkButtonIcon = document.querySelector('.dark-mode-btn i')
 darkButton.addEventListener('click', e => {
   darkButtonIcon.classList.toggle('fas')
 })
+
+
+
 const countries = document.querySelector('.countries')
 const countryList = localStorage.getItem('countryList') ? JSON.parse(localStorage.getItem('countryList')) : []
 const searchInput = document.querySelector('#search')
+
 
 searchInput.addEventListener('keyup', e => {
   const input = searchInput.value.toLowerCase()
   countries.innerHTML = ''
   displayCountries(countryList.filter(e => e.name.toLowerCase().includes(searchInput.value.toLowerCase())))
 })
-
 
 
 const displayCountries = (theList = countryList) => {
@@ -32,7 +35,7 @@ const displayCountries = (theList = countryList) => {
     const name = document.createElement('h2')
     info.appendChild(name)
     name.classList.add('country-name')
-    name.textContent = country.name
+    name.textContent = country.name === 'United Kingdom of Great Britain and Northern Ireland' ? 'United Kingdom' : country.name
     const facts = document.createElement('ul')
     info.appendChild(facts)
     facts.classList.add('facts')
@@ -62,7 +65,8 @@ const getCountries = async () => {
           name: country.name,
           population: country.population,
           region: country.region,
-          capital: country.capital
+          capital: country.capital,
+          region: country.region,
         })
       })
       localStorage.setItem('countryList', JSON.stringify(countryList))
@@ -78,3 +82,88 @@ const getCountries = async () => {
 
 getCountries()
 
+function DropDown(dropDown) {
+  const [toggler, menu] = dropDown.children
+
+  const handleClickOut = e => {
+    if (!dropDown) {
+      return document.removeEventListener('click', handleClickOut)
+    }
+
+    if (!dropDown.contains(e.target)) {
+      this.toggle(false)
+    }
+  }
+
+  const setValue = (item) => {
+    const val = item.textContent
+    toggler.textContent = val
+    this.value = val
+    this.toggle(false)
+    dropDown.dispatchEvent(new Event('change'))
+    toggler.focus()
+
+  }
+
+  const handleItemKeyDown = (e) => {
+    e.preventDefault()
+
+    if (e.keyCode === 38 && e.target.previousElementSibling) { // up
+      e.target.previousElementSibling.focus()
+    } else if (e.keyCode === 40 && e.target.nextElementSibling) { // down
+      e.target.nextElementSibling.focus()
+    } else if (e.keyCode === 27) { // escape key
+      this.toggle(false)
+    } else if (e.keyCode === 13 || e.keyCode === 32) { // enter or spacebar key
+      setValue(e.target)
+    }
+  }
+
+  const handleToggleKeyPress = (e) => {
+    e.preventDefault()
+
+    if (e.keyCode === 27) { // escape key
+      this.toggle(false)
+    } else if (e.keyCode === 13 || e.keyCode === 32) { // enter or spacebar key
+      this.toggle(true)
+    }
+  }
+
+  toggler.addEventListener('keydown', handleToggleKeyPress)
+  toggler.addEventListener('click', () => this.toggle());
+  [...menu.children].forEach(item => {
+    item.addEventListener('keydown', handleItemKeyDown)
+    item.addEventListener('click', () => setValue(item))
+  })
+
+  this.element = dropDown
+
+  this.value = toggler.textContent
+
+  this.toggle = (expand = null) => {
+    expand = expand === null
+      ? menu.getAttribute('aria-expanded') !== 'true'
+      : expand
+
+    menu.setAttribute('aria-expanded', expand)
+
+    if (expand) {
+      toggler.classList.add('active')
+      menu.children[0].focus()
+      document.addEventListener('click', handleClickOut)
+      dropDown.dispatchEvent(new Event('opened'))
+    } else {
+      toggler.classList.remove('active')
+      dropDown.dispatchEvent(new Event('closed'))
+      document.removeEventListener('click', handleClickOut)
+    }
+  }
+}
+
+const dropDown = new DropDown(document.querySelector('.dropdown'))
+
+dropDown.element.addEventListener('change', () => {
+  countries.innerHTML = ''
+  dropDown.value === 'All' ? displayCountries() : displayCountries(countryList.filter(e => e.region === dropDown.value))
+})
+dropDown.toggle()
